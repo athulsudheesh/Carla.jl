1344336ab03cc3a280fb03d30fe10971b47c630b
Athul Sudheesh <athuls0708@gmail.com>
2022-07-20 22:43:56 -0500

    Update gradients.jl

--- src/functions/gradients.jl
+++ src/functions/gradients.jl
@@ -3,6 +3,8 @@
 Computes the gradient of the emperical risk for a given student 
 """
 function Δriskᵢ(
-            model::CPM, data::StudentResponse)
-            
+            model::CPM, data::StudentResponse, αMatrix)
+    
+    nrskills, nrtimepoints = size(αMatrix)
+    nritems, _ = size()
 end
